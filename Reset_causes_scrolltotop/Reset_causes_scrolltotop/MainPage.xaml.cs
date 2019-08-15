2a6a355f50c0624788d775a1e89835b75559a42b
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2019-08-15 16:05:33 +0200

    -init

---
+++ Reset_causes_scrolltotop/Reset_causes_scrolltotop/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Reset_causes_scrolltotop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
